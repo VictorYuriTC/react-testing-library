348bdce5e856c41f2d50d40d46cd1ae2ddb5eca7
Victor Yuri <victoryuritc@yahoo.com>
2022-07-18 19:18:28 -0300

    test About page. test if the h2 About Pokédex is rendered. test if the document rendered the expected image

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,39 +1,20 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import App from '../App';
+import { About } from '../pages';
 import renderWithRouter from '../helpers/renderWithRouter';
 
 describe('about page', () => {
-  const { history } = renderWithRouter(<App />);
-  const homeLink = screen.getByRole('link', { name: /home/i });
-  const aboutLink = screen.getByRole('link', { name: /about/i });
-  const favoritesLink = screen.getByRole('link', { name: /favorite pokémons/i });
+  const { history } = renderWithRouter(<About />);
+  const img = screen.getByAltText(/pokédex/i);
 
-  test('if home, about and favoritePokemons texts are in the document', () => {
-    expect(homeLink).toHaveTextContent(/home/i);
-    expect(aboutLink).toHaveTextContent(/about/i);
-    expect(favoritesLink).toHaveTextContent(/favorite pokémons/i);
+  test('if it contains a heading h2 with the expected text', () => {
+    const header = screen.getByRole('heading', { level: 2, name: /About Pokédex/i });
+    expect(header).toBeInTheDocument();
   });
 
-  test('if user is redirect to Home when clicking on link', () => {
-    const { pathname } = history.location;
-    expect(pathname).toBe('/');
-
-    userEvent.click(homeLink);
-
-    expect(pathname).toBe('/');
-  });
-
-  test('if user is redirect to about when clicking on link', () => {
-    userEvent.click(aboutLink);
-    history.push('/about');
-    expect(history.location.pathname).toBe('/about');
-  });
-
-  test('if user is redirect to favorites when clicking on link', () => {
-    userEvent.click(favoritesLink);
-    history.push('/favorites');
-    expect(history.location.pathname).toBe('/favorites');
+  test('if there is the expected image in the page', () => {
+    console.log(history.location.pathname);
+    const URL = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    expect(img.src).toContain(URL);
   });
 });
