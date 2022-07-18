ec88bc899946312a9502f622dfa6a197a795fd47
Victor Yuri <victoryuritc@yahoo.com>
2022-07-18 18:38:13 -0300

    Links tests created. test if user is redirected to Home, About and Favorites when clicking on respective Links. test if they have the expected texts

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,41 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../helpers/renderWithRouter';
+
+// João Nascimento helped me, since for some reason { pathname } was not working
+
+describe('testing app', () => {
+  const { history } = renderWithRouter(<App />);
+  const homeLink = screen.getByRole('link', { name: /home/i });
+  const aboutLink = screen.getByRole('link', { name: /about/i });
+  const favoritesLink = screen.getByRole('link', { name: /favorite pokémons/i });
+
+  test('if home, about and favoritePokemons texts are in the document', () => {
+    expect(homeLink).toHaveTextContent(/home/i);
+    expect(aboutLink).toHaveTextContent(/about/i);
+    expect(favoritesLink).toHaveTextContent(/favorite pokémons/i);
+  });
+
+  test('if user is redirect to Home when clicking on link', () => {
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+
+    userEvent.click(homeLink);
+
+    expect(pathname).toBe('/');
+  });
+
+  test('if user is redirect to about when clicking on link', () => {
+    userEvent.click(aboutLink);
+    history.push('/about');
+    expect(history.location.pathname).toBe('/about');
+  });
+
+  test('if user is redirect to favorites when clicking on link', () => {
+    userEvent.click(favoritesLink);
+    history.push('/favorites');
+    expect(history.location.pathname).toBe('/favorites');
+  });
+});
