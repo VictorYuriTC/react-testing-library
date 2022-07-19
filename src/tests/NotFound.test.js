cc0834f5c8447392d79e8011fca5739ae3565846
Victor Yuri <victoryuritc@yahoo.com>
2022-07-18 21:15:27 -0300

    NotFound page suite tests created. test if the expected h2 is rendered. test if the expected image is rendered.

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+import renderWithRouter from '../helpers/renderWithRouter';
+
+describe('NotFound page suite tests', () => {
+  renderWithRouter(<NotFound />);
+  const image = screen
+    .getByAltText(/Pikachu crying because the page requested was not found/i);
+  test('if it contains a h2 with the expected text', () => {
+    const HEADER = screen.getByRole('heading',
+      { level: 2, name: /Page requested not found/i });
+    expect(HEADER).toBeInTheDocument();
+  });
+  test('if the expected image is rendered at the page', () => {
+    const URL = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    expect(image.src).toContain(URL);
+  });
+});
