130d41c4a39029d9b2d1e69eb8a0bbbe03dbe68f
Victor Yuri <victoryuritc@yahoo.com>
2022-08-06 14:15:40 -0300

    Pokedex suite tests concluded. find used in order to get typeButton with a specified HTML, userEvent click on it and, then, expects used with toHaveTextContent expecting the same type of pokemon. project finished

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -11,18 +11,87 @@ describe('Pokedex page suite tests', () => {
     const h2ExpectedText = /encountered pokémons/i;
     expect(h2).toHaveTextContent(h2ExpectedText);
   });
-  it('should show the next pokemon after clicking', () => {
-    renderWithRouter(<App />);
-    const button = screen.getByText(/próximo pokémon/i);
 
-    expect(button).toBeInTheDocument();
+  it('should show the next pokemon after clicking on próximo pokémon', () => {
+    renderWithRouter(<App />);
+
+    const nextPokemonButton = screen.getByText(/próximo pokémon/i);
+    const firstPokemon = screen.getByText(/pikachu/i);
+
+    expect(nextPokemonButton).toBeInTheDocument();
+    expect(firstPokemon).toBeInTheDocument();
+
+    userEvent.click(nextPokemonButton);
+    const nextPokemon = screen.getByText(/charmander/i);
+
+    expect(nextPokemon).toBeInTheDocument();
+
+    const psychicButton = screen.getByRole('button', { name: 'Psychic' });
+    userEvent.click(psychicButton);
+    const firstDifferentTypePokemon = screen.getByText(/alakazam/i);
+    const firstDifferentTypePokemonType = screen.getByTestId('pokemon-type');
+
+    expect(firstDifferentTypePokemon).toBeInTheDocument();
+    expect(psychicButton).toHaveTextContent(/psychic/i);
+    expect(firstDifferentTypePokemonType).toHaveTextContent(/psychic/i);
+
+    userEvent.click(nextPokemonButton);
+    const nextDifferentTypePokemon = screen.getByText(/mew/i);
+
+    expect(nextDifferentTypePokemon);
+
+    userEvent.click(nextPokemonButton);
+
+    expect(firstDifferentTypePokemon);
+
+    const allTypesOfPokemonButton = screen.getByRole('button', { name: 'All' });
+    userEvent.click(allTypesOfPokemonButton);
+
+    expect(firstPokemon).toBeInTheDocument();
   });
+
+  it('should show the first pokémon after clicking on next pokemon when expected', () => {
+    renderWithRouter(<App />);
+
+    const nextPokemonButton = screen.getByRole('button', { name: /próximo pokémon/i });
+    userEvent.click(nextPokemonButton);
+    userEvent.click(nextPokemonButton);
+    userEvent.click(nextPokemonButton);
+    userEvent.click(nextPokemonButton);
+    userEvent.click(nextPokemonButton);
+    userEvent.click(nextPokemonButton);
+    userEvent.click(nextPokemonButton);
+    userEvent.click(nextPokemonButton);
+
+    const lastPokemon = screen.getByText(/dragonair/i);
+
+    expect(lastPokemon).toBeInTheDocument();
+
+    userEvent.click(nextPokemonButton);
+
+    const firstPokemon = screen.getByText(/pikachu/i);
+
+    expect(firstPokemon).toBeInTheDocument();
+  });
+
   it('should have type filter buttons on screen', () => {
     renderWithRouter(<App />);
     const button = screen.getByRole('button', { name: 'Fire' });
 
     userEvent.click(button);
   });
+
+  it('should match pokemon type and pokemon type button texts', () => {
+    renderWithRouter(<App />);
+    const psychicButton = screen.getAllByTestId('pokemon-type-button')
+      .find((typeButton) => typeButton.innerHTML.includes('Psychic'));
+    userEvent.click(psychicButton);
+
+    const firstPsychicPokemonType = screen.getByTestId('pokemon-type');
+    expect(psychicButton).toHaveTextContent(/psychic/i);
+    expect(firstPsychicPokemonType).toHaveTextContent(/psychic/i);
+  });
+
   it('should have a button to reset filtering', () => {
     renderWithRouter(<App />);
     const button = screen.getByText(/all/i);
